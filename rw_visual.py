f1806e1541f962ebb6886c3e4c4191f697e10b93
Harsh Chilhate <abitharshchilhate@gmail.com>
2025-12-28 01:03:53 +0530

    Start point and end point highlights

--- rw_visual.py
+++ rw_visual.py
@@ -14,4 +14,7 @@ point_numbers = range(rw.num_points)
 ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors = 'none', vmin=0, vmax = rw.num_points)
 ax.set_aspect('equal')
 
+#Emphasis the first and last points.
+ax.scatter(0, 0, c = 'green', edgecolors='none', s = 100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100 )
 plt.show()
